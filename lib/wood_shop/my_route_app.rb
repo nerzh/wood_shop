4ed6235d62e6df913e34d1cdb663482a49bd8424
woodcrust <roboucrop@gmail.com>
2017-08-15 15:55:30 +0300

    my routing

---
+++ lib/wood_shop/my_route_app.rb
@@ -0,0 +1,21 @@
+class MyRouteApp
+
+  attr_reader :controller
+  attr_accessor :http_path_info
+
+  def initialize(controller)
+    @controller = controller
+  end
+
+  def matches?(request)
+    request.env['REQUEST_PATH'][/\/([^\.$]+)/]
+    self.http_path_info = $1.sub(/\/$/, '')
+
+    if url = WoodShop::FriendlyIdSlug.find_by( slug: self.http_path_info )
+      return true if url.sluggable_type == controller
+    end
+
+    false
+  end
+
+end
