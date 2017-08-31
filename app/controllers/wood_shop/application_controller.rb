2615b02b1ab708eceeb176cb47452ca09bf3e50d
woodcrust <roboucrop@gmail.com>
2017-08-31 04:47:24 +0300

    fix layout

--- app/controllers/wood_shop/application_controller.rb
+++ app/controllers/wood_shop/application_controller.rb
@@ -1,5 +1,7 @@
 module WoodShop
   class ApplicationController < ActionController::Base
+    # layout 'wood_shop/application'
+    layout 'application'
     protect_from_forgery with: :exception
   end
 end
