0779683210708f31435d240ddcbb0878ba245b0e
woodcrust <roboucrop@gmail.com>
2017-09-08 20:48:34 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 WoodShop::Engine.routes.draw do
-  WoodShop::Admin = Admin
+  WoodShop::Admin = Admin unless defined? WoodShop::Admin
 
   devise_for :users, class_name: "WoodShop::User", module: :devise
   devise_for :admin_users, ActiveAdmin::Devise.config.merge( {class_name: "WoodShop::AdminUser"} )
@@ -13,10 +13,10 @@ WoodShop::Engine.routes.draw do
 
   root to: 'index#index'
 
-  resources :catalogs,   only: [:show]
-  resources :categories, only: [:index, :show]
-  resources :products,   only: [:show]
-  resources :brands,     only: [:index, :show]
+  scope :api do
+    api_version(module: "Api::v1", path: {:value => "v1"}, default: true) do
+    end
+  end
 
   # resources :update do
   #   collection do
@@ -36,14 +36,18 @@ WoodShop::Engine.routes.draw do
   #   end
   # end
 
-  CATALOG  = 'Catalog'
-  CATEGORY = 'Category'
-  PRODUCT  = 'Product'
-  BRAND    = 'Brand'
-  match '/*id', to: 'catalogs#show',   constraints: MyRouteApp.new(CATALOG), via: :get
-  match '/*id', to: 'categories#show', constraints: MyRouteApp.new(CATEGORY), via: :get
-  match '/*id', to: 'products#show',   constraints: MyRouteApp.new(PRODUCT),  via: :get
-  match '/*id', to: 'brands#show',     constraints: MyRouteApp.new(BRAND),    via: :get
+  # CATALOG  = 'WoodShop::Catalog'
+  # CATEGORY = 'WoodShop::Category'
+  # match '/*id', to: 'catalogs#show',   constraints: MyRouteApp.new(CATALOG), via: :get
+  # match '/*id', to: 'categories#show', constraints: MyRouteApp.new(CATEGORY), via: :get
+  PRODUCT = 'WoodShop::Product' unless defined? PRODUCT
+  # BRAND    = 'WoodShop::Brand'
+  match '/:catalog_id/categories',     to: 'categories#index',  via: :get, as: 'categories'
+  match '/:catalog_id/categories/:id', to: 'categories#show',   via: :get, as: 'category'
+  match '/*id', to: 'products#show',   constraints: MyRouteApp.new(PRODUCT),  via: :get, as: 'product'
+  # match '/*id', to: 'brands#show',     constraints: MyRouteApp.new(BRAND),    via: :get, as: 'brand'
+
+  resources :brands,     only: [:index, :show]
 
   get 'errors/not_found'
   get 'errors/internal_server_error'
--- lib/wood_shop/my_route_app.rb
+++ lib/wood_shop/my_route_app.rb
@@ -9,7 +9,8 @@ class MyRouteApp
 
   def matches?(request)
     request.env['REQUEST_PATH'][/\/([^\.$]+)/]
-    self.http_path_info = $1.sub(/\/$/, '')
+    self.http_path_info = $1
+    self.http_path_info.sub!(/\/$/, '')
 
     if url = WoodShop::FriendlyIdSlug.find_by( slug: self.http_path_info )
       return true if url.sluggable_type == controller
